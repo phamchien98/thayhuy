ea06fe0ac54c4e6c81bc7020732470cbe96b9618
LeHuyHung <LeHuyHung>
2020-01-09 22:05:33 +0700

    a

--- test.c
+++ test.c
@@ -112,6 +112,7 @@ int main()
   // chiem ham la nguoi yeu hung
 	printf("neu tach mon thi chia tay nhe !!!");
 	printf("toi may em khong di day thi minh di choi nhe nhe !!!");
+	printf("anh nghi anh nen doi luong !!!");
 
 
   treenode  *root=NULL;
